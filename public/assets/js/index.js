bf589080d01700f025bc2fa6d692d8c48fd0c10c
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-03-22 00:28:04 +0100

    add the land page

---
+++ public/assets/js/index.js
@@ -0,0 +1,12 @@
+ const hamburguerHeaderBTN = document.querySelector("#hamburguerHeader");
+
+ if (hamburguerHeaderBTN !== null) {
+     hamburguerHeaderBTN.addEventListener("click", () => {
+         document.querySelector(".menuContainer").classList.toggle("show");
+         if(document.querySelector(".menuContainer").classList.contains("show")){
+             hamburguerHeaderBTN.src="/assets/img/x.svg";
+            }else{
+                hamburguerHeaderBTN.src="/assets/img/hamburguer.svg";    
+         }
+     });
+ }
